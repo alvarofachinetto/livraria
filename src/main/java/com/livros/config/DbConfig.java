b02683ca527690d99e8f21aacf8768743f88a08d
Alvaro Silva <alvaro.facneto@gmail.com>
2019-12-15 20:12:44 -0300

    Configuracao carga de dados e criacao de profile teste

---
+++ src/main/java/com/livros/config/DbConfig.java
@@ -0,0 +1,24 @@
+package com.livros.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import com.livros.service.H2Service;
+
+@Profile("teste")
+@Configuration
+public class DbConfig {
+	
+	@Autowired
+	private H2Service h2Service;
+	
+	@Bean
+	public boolean instantiateDatabase() throws Exception {
+		h2Service.loadH2();
+		return true;
+	}
+	
+	
+}
---
+++ src/main/java/com/livros/service/H2Service.java
@@ -0,0 +1,28 @@
+package com.livros.service;
+
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.livros.model.Livro;
+import com.livros.repository.LivroRepository;
+
+@Service
+public class H2Service {
+	
+	@Autowired
+	private LivroRepository livroRepository;
+	
+	public void loadH2() throws Exception{
+		Livro [] livros = {
+			new Livro(null, "50 Tons de Cinza", "E. L. James", "Vitage Books", 110, 19.90),
+			new Livro(null, "Pedro Nava - Leitor de Drummond", "Pedro Lenza", "Saraiva", 95, 25.90),
+			new Livro(null, "Do Luto ao Nascimento", "Camila Cabral", "Saraiva", 160, 30.00),
+			new Livro(null, "Um Caminho Para A Liberdade", "Jojo Moyes", "Saraiva", 368, 39.90)
+		};
+		
+		Iterable<Livro> livrosIt = Arrays.asList(livros);
+		livroRepository.saveAll(livrosIt);
+	}
+}
