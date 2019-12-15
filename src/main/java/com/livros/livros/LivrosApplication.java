1f643a278d6baca8d6fe5a3afebf561f59a772a6
Alvaro Silva <alvaro.facneto@gmail.com>
2019-12-15 00:25:44 -0300

    configuracao do main

--- src/main/java/com/livros/livros/LivrosApplication.java
+++ src/main/java/com/livros/livros/LivrosApplication.java
@@ -2,8 +2,12 @@ package com.livros.livros;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan("com.livros")
+@EntityScan("com.livros.model")
 public class LivrosApplication {
 
 	public static void main(String[] args) {
