aec824bdc9ba27cfc4b01b7efb97d2bc335ed952
Alvaro Silva <alvaro.facneto@gmail.com>
2019-12-15 00:28:09 -0300

    adicao de entidade livro

---
+++ src/main/java/com/livros/model/Livro.java
@@ -0,0 +1,105 @@
+package com.livros.model;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Livro implements Serializable{
+
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long idLivro;
+	
+	@Column(length = 60)
+	private String titulo;
+	@Column(length = 60)
+	private String autor;
+	@Column(length = 40)
+	private String editora;
+	private Integer paginas;
+	@Column(precision = 10, scale = 2)
+	private Double preco;
+	
+	public Livro() {}
+	
+	public Livro(Long idLivro, String titulo, String autor, String editora, Integer paginas, Double preco) {
+		super();
+		this.idLivro = idLivro;
+		this.titulo = titulo;
+		this.autor = autor;
+		this.editora = editora;
+		this.paginas = paginas;
+		this.preco = preco;
+	}
+	public Long getIdLivro() {
+		return idLivro;
+	}
+	public void setIdLivro(Long idLivro) {
+		this.idLivro = idLivro;
+	}
+	public String getTitulo() {
+		return titulo;
+	}
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+	public String getAutor() {
+		return autor;
+	}
+	public void setAutor(String autor) {
+		this.autor = autor;
+	}
+	public String getEditora() {
+		return editora;
+	}
+	public void setEditora(String editora) {
+		this.editora = editora;
+	}
+	public Integer getPaginas() {
+		return paginas;
+	}
+	public void setPaginas(Integer paginas) {
+		this.paginas = paginas;
+	}
+	public Double getPreco() {
+		return preco;
+	}
+	public void setPreco(Double preco) {
+		this.preco = preco;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((idLivro == null) ? 0 : idLivro.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Livro other = (Livro) obj;
+		if (idLivro == null) {
+			if (other.idLivro != null)
+				return false;
+		} else if (!idLivro.equals(other.idLivro))
+			return false;
+		return true;
+	}
+	
+	
+	
+}
